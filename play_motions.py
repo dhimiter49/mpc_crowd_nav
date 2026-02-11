202c261923d3317eb46429c0cb9d826c13ac85c6
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2026-02-11 17:14:38 +0100

    show all trajectories and execute shortest

--- play_motions.py
+++ play_motions.py
@@ -45,7 +45,10 @@ elif "-mcco" in sys.argv:
 else:
     env_type = ENV_DICT["-d"]
     env_str = "Navigation%s-v0" % velocity_str
+
+mult_plan = 1 if "-mp" not in sys.argv else int(sys.argv[sys.argv.index("-mp") + 1])
 env = gym.make("fancy/" + env_str)
+DT = env.unwrapped.dt
 # env = gym.wrappers.RecordVideo(
 #     env=env,
 #     video_folder="/home/dhimiter/Videos",
@@ -56,8 +59,9 @@ env.reset()
 n_crowd = env.unwrapped.n_crowd
 # env.start_video_recorder()
 
-print("There are " + str(len(motion_data)) + " trajectories to animate.")
-for trajectory_data in motion_data:
+print("There are " + str(len(motion_data) // mult_plan) + " episodes.")
+for i in range(0, len(motion_data), mult_plan):
+    trajectory_data = motion_data[i]
     agent_pos = trajectory_data[:2]
     crowd_poss = trajectory_data[2:2 + n_crowd * 2]
     agent_vel = trajectory_data[2 + n_crowd * 2:4 + n_crowd * 2]
@@ -73,10 +77,33 @@ for trajectory_data in motion_data:
         }
     )
     horizon = len(trajectory_data[6 + n_crowd * 4:]) // 4
-    plan = trajectory_data[6 + n_crowd * 4:6 + n_crowd * 4 + horizon * 2]
+    all_actions = [
+        motion_data[i + j][6 + n_crowd * 4 + horizon * 2:].reshape(-1, 2)
+        for j in range(mult_plan)
+    ]
+    positions = [
+        np.concatenate([np.array([[0, 0]]), np.cumsum(a * DT, axis=0)])
+        for a in all_actions
+    ]
+    dist = np.array([np.sum(np.linalg.norm(p[1:] - p[:-1], axis=-1)) for p in positions])
+    non_stat_idx = np.where(dist > 1e-5)[0]
+    to_goal_idx = np.where(
+        np.linalg.norm(np.array(positions)[:, -1] + agent_pos - goal_pos, axis=-1) < 0.4
+    )[0]
+    valid_idx = list(set(non_stat_idx) & set(to_goal_idx))
+    if len(valid_idx) == 0:
+        continue
+
+    dist = dist[valid_idx]
+    positions = np.array(positions)[valid_idx]
+    all_actions = np.array(all_actions)[valid_idx]
+    sorted_dist = np.argsort(dist)
+    env.get_wrapper_attr("set_all_motions")(np.array(positions)[np.argsort(dist)])
+
+    plan = motion_data[i + sorted_dist[0]][6 + n_crowd * 4:6 + n_crowd * 4 + horizon * 2]
     plan = np.array([plan[:len(plan) // 2], plan[len(plan) // 2:]]).T
     env.get_wrapper_attr("set_trajectory")(plan)
-    actions = trajectory_data[6 + n_crowd * 4 + horizon * 2:].reshape(-1, 2)
+    actions = all_actions[sorted_dist[0]]
     for a in actions:
         env.render()
         obs, reward, terminated, truncated, info = env.step(a)
